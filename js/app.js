2278199a90e58805d8c503812612472eb9206e2d
Vasudeva Pitta <38531141+vasudevapitta@users.noreply.github.com>
2018-05-30 15:23:24 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -151,7 +151,6 @@ allCards.forEach(function(card){
 									TIME TAKEN ${minsFirstZero}${mins}:${secsFirstZero}${secs}
 									IN ${moves} MOVES<br><p class="playAgain">PLAY AGAIN</p>`;
 									result.style.visibility="visible";
-									confettiAnimation();
 								};
 								openCards=[]; //openCards array is emptied
 								currentOpenCards=0;
@@ -195,116 +194,6 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-function confettiAnimation(){
-document.querySelector("#canvas").style.visibility="visible";
-let W = window.innerWidth;
-let H = window.innerHeight;
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-const maxConfettis = 150;
-const particles = [];
-
-const possibleColors = [
-  "DodgerBlue",
-  "OliveDrab",
-  "Gold",
-  "Pink",
-  "SlateBlue",
-  "LightBlue",
-  "Gold",
-  "Violet",
-  "PaleGreen",
-  "SteelBlue",
-  "SandyBrown",
-  "Chocolate",
-  "Crimson"
-];
-
-function randomFromTo(from, to) {
-  return Math.floor(Math.random() * (to - from + 1) + from);
-}
-
-function confettiParticle() {
-const playAgain = document.querySelector(".playAgain");
-playAgain.addEventListener("click", reload);
-
-  this.x = Math.random() * W; // x
-  this.y = Math.random() * H - H; // y
-  this.r = randomFromTo(11, 33); // radius
-  this.d = Math.random() * maxConfettis + 11;
-  this.color =
-    possibleColors[Math.floor(Math.random() * possibleColors.length)];
-  this.tilt = Math.floor(Math.random() * 33) - 11;
-  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
-  this.tiltAngle = 0;
-
-  this.draw = function() {
-    context.beginPath();
-    context.lineWidth = this.r / 2;
-    context.strokeStyle = this.color;
-    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
-    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
-    return context.stroke();
-  };
-}
-
-function Draw() {
-  const results = [];
-
-  // Magical recursive functional love
-  requestAnimationFrame(Draw);
-
-  context.clearRect(0, 0, W, window.innerHeight);
-
-  for (var i = 0; i < maxConfettis; i++) {
-    results.push(particles[i].draw());
-  }
-
-  let particle = {};
-  let remainingFlakes = 0;
-  for (var i = 0; i < maxConfettis; i++) {
-    particle = particles[i];
-
-    particle.tiltAngle += particle.tiltAngleIncremental;
-    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
-    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
-
-    if (particle.y <= H) remainingFlakes++;
-
-    // If a confetti has fluttered out of view,
-    // bring it back to above the viewport and let if re-fall.
-    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
-      particle.x = Math.random() * W;
-      particle.y = -30;
-      particle.tilt = Math.floor(Math.random() * 10) - 20;
-    }
-  }
-
-  return results;
-}
-
-window.addEventListener(
-  "resize",
-  function() {
-    W = window.innerWidth;
-    H = window.innerHeight;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  },
-  false
-);
-
-// Push new confetti objects to `particles[]`
-for (var i = 0; i < maxConfettis; i++) {
-  particles.push(new confettiParticle());
-}
-
-// Initialize
-canvas.width = W;
-canvas.height = H;
-Draw();
-}
 
 const endTime = performance.now();
 console.log(endTime-startTime);
