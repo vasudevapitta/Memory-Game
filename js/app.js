27c445c4546102fd1623b1f770510e383ef9b28e
Vasudeva Pitta <38531141+vasudevapitta@users.noreply.github.com>
2018-06-01 14:12:56 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,23 +2,24 @@
  * Create a list that holds all of your cards
  */
 
+ const startTime = performance.now(); // start time to calculate the total load time
+
+//function to reload the window - To replay the game
  function reload(){
 	location.reload();
  }
 
- let user = window.prompt("Please enter your name");
+ let user = window.prompt("Please enter your name"); //Storing the name of the player
 
- const result = document.querySelector(".result");
+ const result = document.querySelector(".result"); //for result slide which comes at the end of the game
 
- const stars = document.querySelector(".stars");
+ const stars = document.querySelector(".stars"); //3 Stars
 
  const firstStar = stars.querySelectorAll("i")[0];
 
  const secondStar = stars.querySelectorAll("i")[1];
 
  const thirdStar = stars.querySelectorAll("i")[2];
-
- const startTime = performance.now();
 
  const repeat = document.querySelector(".fa-repeat");
 
@@ -68,7 +69,7 @@ function initGame(){
 initGame();
 
 function generateCard(card){
-	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`; 
 }
 
 const allCards=document.querySelectorAll(".card");
@@ -151,6 +152,7 @@ allCards.forEach(function(card){
 									TIME TAKEN ${minsFirstZero}${mins}:${secsFirstZero}${secs}
 									IN ${moves} MOVES<br><p class="playAgain">PLAY AGAIN</p>`;
 									result.style.visibility="visible";
+									confettiAnimation();
 								};
 								openCards=[]; //openCards array is emptied
 								currentOpenCards=0;
@@ -195,5 +197,118 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-const endTime = performance.now();
-console.log(endTime-startTime);
+
+
+function confettiAnimation(){
+const playAgain = document.querySelector(".playAgain");
+playAgain.addEventListener("click", reload);
+
+document.querySelector("#canvas").style.visibility="visible";
+let W = window.innerWidth;
+let H = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const maxConfettis = 150;
+const particles = [];
+
+const possibleColors = [
+  "DodgerBlue",
+  "OliveDrab",
+  "Gold",
+  "Pink",
+  "SlateBlue",
+  "LightBlue",
+  "Gold",
+  "Violet",
+  "PaleGreen",
+  "SteelBlue",
+  "SandyBrown",
+  "Chocolate",
+  "Crimson"
+];
+
+function randomFromTo(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function confettiParticle() {
+
+  this.x = Math.random() * W; // x
+  this.y = Math.random() * H - H; // y
+  this.r = randomFromTo(11, 33); // radius
+  this.d = Math.random() * maxConfettis + 11;
+  this.color =
+    possibleColors[Math.floor(Math.random() * possibleColors.length)];
+  this.tilt = Math.floor(Math.random() * 33) - 11;
+  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+  this.tiltAngle = 0;
+
+  this.draw = function() {
+    context.beginPath();
+    context.lineWidth = this.r / 2;
+    context.strokeStyle = this.color;
+    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+    return context.stroke();
+  };
+}
+
+function Draw() {
+  const results = [];
+
+  // Magical recursive functional love
+  requestAnimationFrame(Draw);
+
+  context.clearRect(0, 0, W, window.innerHeight);
+
+  for (var i = 0; i < maxConfettis; i++) {
+    results.push(particles[i].draw());
+  }
+
+  let particle = {};
+  let remainingFlakes = 0;
+  for (var i = 0; i < maxConfettis; i++) {
+    particle = particles[i];
+
+    particle.tiltAngle += particle.tiltAngleIncremental;
+    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+    if (particle.y <= H) remainingFlakes++;
+
+    // If a confetti has fluttered out of view,
+    // bring it back to above the viewport and let if re-fall.
+    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+      particle.x = Math.random() * W;
+      particle.y = -30;
+      particle.tilt = Math.floor(Math.random() * 10) - 20;
+    }
+  }
+
+  return results;
+}
+
+window.addEventListener(
+  "resize",
+  function() {
+    W = window.innerWidth;
+    H = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  },
+  false
+);
+
+// Push new confetti objects to `particles[]`
+for (var i = 0; i < maxConfettis; i++) {
+  particles.push(new confettiParticle());
+}
+
+// Initialize
+canvas.width = W;
+canvas.height = H;
+Draw();
+}
+
+const endTime = performance.now(); // end time to calculate the total load time
+console.log(endTime-startTime); //logs out the total time taken to load the DOM
